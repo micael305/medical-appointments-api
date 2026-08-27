52c668ea24cbb6e4c7acb137f282a316f5ecc2b9
micael305 <miicael305@gmail.com>
2026-08-27 11:58:15 -0300

    chore: initialize Prisma client with pg adapter and delete output in generator client

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -1,11 +1,17 @@
 import 'dotenv/config';
+import pg from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../../generated/prisma/index.js';
+import prismaPkg from '@prisma/client';
+const { PrismaClient } = prismaPkg;
+
+const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL
+});
+
+const adapter = new PrismaPg(pool);
 
 const prisma = new PrismaClient({
-    adapter: new PrismaPg({
-        connectionString: process.env.DATABASE_URL
-    })
+    adapter
 });
 
 export default prisma;
